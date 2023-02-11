004db2b8c023937edbb716c40bf414c570cd127c
Froxzen <112826187+Froxzen@users.noreply.github.com>
2023-02-11 18:00:35 +0100

    Create network.py

---
+++ network.py
@@ -0,0 +1,13 @@
+from cryptography.fernet import Fernet
+
+#Encrypt any data that goes through this function
+def encrypt(data):
+    key = Fernet.generate_key()
+    fernet = Fernet(key)
+    encMessage = fernet.encrypt(data.encode())
+    return encMessage
+  
+#E any data that goes through this function
+def decrypt(data):
+    decMessage = fernet.decrypt(data).decode()
+    return decMessage

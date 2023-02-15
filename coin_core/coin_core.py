9a87c54de3e9598e6e3eeb541b72f06cb23a1862
M <112826187+m-the-coder@users.noreply.github.com>
2023-02-15 09:46:04 +0000

    started working on the blockchain comunication

--- coin_core/coin_core.py
+++ coin_core/coin_core.py
@@ -1,8 +1,9 @@
 import hashlib
 import datetime
 import time
+from 
 
-class Cabbage:
+class Block:
 
     def __init__(self, data, previous_hash):
         self.timestamp = datetime.datetime.now()
@@ -15,7 +16,7 @@ class Cabbage:
         target = '0' * difficulty + '1' * (64 - difficulty)
         nonce = 0
         while True:
-            data = f"Cabbage network is the best{int(time.time())}{nonce}"
+            data = f"PRC{int(time.time())}{nonce}"
             result = hashlib.sha256(data.encode()).hexdigest()
             if result[:difficulty] == target[:difficulty]:
                 return result
@@ -27,11 +28,11 @@ class Blockchain:
         self.chain = [self.create_genesis_block()]
 
     def create_genesis_block(self):
-        return Cabbage("Genesis Block", "0")
+        return Block("Genesis Block", "0")
 
     def add_block(self, data):
         previous_block = self.chain[-1]
-        new_block = Cabbage(data, previous_block.hash)
+        new_block = Block(data, previous_block.hash)
         self.chain.append(new_block)
 
     def print_chain(self):

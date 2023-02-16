5c38ce3746dfbb2f02fb9fd7bcfb57674b3c22b4
M <112826187+m-the-coder@users.noreply.github.com>
2023-02-16 14:39:04 +0100

    working on the coin core

--- coin_core.py
+++ coin_core.py
@@ -1,33 +1,47 @@
 import hashlib
-import random
-import string
-import os
+import calendar
+import time
+current_GMT = time.gmtime()
+time_stamp = calendar.timegm(current_GMT)
 
-def update_user_key(key, new_key, priv_key):
-    key_path = "/user_data/user_keys/" + priv_key + ".txt"
-    key_exist = os.path.exists(key_path)
-    with open("/userdata/private_keys.txt") as keys:
-        content = keys.read()
-        if priv_key in content & key_exist == True:
-            f = open(key_path, "w")
-            f.write(new_key)
-            f.close()
-        else:
-            print("Wrong pivate key or user key")
-
-def create_user_identification(username, password):
+def generate_user_identification():
     wallet_balance = 0
-    combined_data = username + " " + password + " "
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    combined_data = username + " " + password + " " + wallet_balance
 
-    private_key = "testkey"
     user_key = hashlib.sha256(combined_data.encode()).hexdigest()
+    private_key = hashlib.sha256(time_stamp.encode()).hexdigest()
 
-    key_path = "/user_data/user_keys/" + private_key + ".txt"
-    f = open(key_path, "w")
-    f.write(user_key)
-    f.close()
-    
-    print("YOUR USER KEY IS: ", user_key)
-    print("YOUR PRIVATE KEY IS: ", private_key)
+    user_key_writer = open("/user_data/user_keys.txt", "a")
+    user_key_writer.write(user_key)
+    private_key_writer.write("\n")
+    user_key_writer.close()
 
-create_user_identification("Testuser", "123")
+    private_key_writer = open("/user_data/user_keys.txt", "a")
+    private_key_writer.write(private_key)
+    private_key_writer.write("\n")
+    private_key_writer.close()
+
+    print("YOUR USER KEY: ", user_key)
+    print("YOUR PRIVATE KEY: ", private_key)
+
+def update_user_identification(user_key, new_user_key, private_key):
+    with open("/user_data/private_keys.txt", 'r') as priv_keys:
+        with open("/user_data/user_keys.txt", r) as usr_keys:
+
+            private_keys = priv_keys.read()
+            user_keys = usr_keys.read()
+            if private_key in private_keys and user_key in user_keys:
+                user_keys.close()
+                file = open("/user_data/user_keys.txt", "r")
+                replaced_content = ""
+                for line in file:
+                    line = line.strip()
+                    new_line = line.replace(user_key, new_user_key)
+                    replaced_content = replaced_content + new_line + "\n"
+                file.close()
+
+                write_file = open("/user_data/user_keys.txt", "w")
+                write_file.write(replaced_content)
+                write_file.close()

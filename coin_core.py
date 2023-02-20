34d4554bb3313b115d3a4786f5568051fda38c45
M <112826187+m-the-coder@users.noreply.github.com>
2023-02-20 20:46:26 +0000

    stff

--- coin_core.py
+++ coin_core.py
@@ -1,53 +1,35 @@
 import hashlib
-import calendar
-import time
-current_GMT = time.gmtime()
-time_stamp = calendar.timegm(current_GMT)
 
-def generate_user_identification():
+def generate_user_id():
     wallet_balance = "0"
     username = input("Enter username: ")
     password = input("Enter password: ")
-    combined_data = username + " " + password + " " + wallet_balance
+    
+    username_n_password = username + password
+    user_key_prefix = hashlib.sha256(username_n_password.encode()).hexdigest()
+    user_key = user_key_prefix + "_" + wallet_balance
+    
+    print(user_key)
 
-    user_key = hashlib.sha256(combined_data.encode()).hexdigest()
-    private_key = hashlib.sha256(user_key.encode()).hexdigest()
 
-    user_key_writer = open("", "a")
-    user_key_writer.write(user_key)
-    private_key_writer.write("\n")
-    user_key_writer.close()
 
-    private_key_writer = open("", "a")
-    private_key_writer.write(private_key)
-    private_key_writer.write("\n")
-    private_key_writer.close()
+def send():
+    sender_key = input("Enter sender key: ")
+    reciver_key = input("Enter reciver key: ")
+    amount = input("Enter amount to send: ")
 
-    print("YOUR USER KEY: ", user_key)
-    print("YOUR PRIVATE KEY: ", private_key)
+    res = sender_key.split("_", 1)
+    sender_balance = res[1]
 
-def update_user_identification(user_key, new_user_key, private_key):
-    with open("", 'r') as priv_keys:
-        with open("", r) as usr_keys:
-            private_keys = priv_keys.read()
-            user_keys = usr_keys.read()
-            if private_key in private_keys and user_key in user_keys:
-                user_keys.close()
-                file = open("", "r")
-                replaced_content = ""
-                for line in file:
-                    line = line.strip()
-                    new_line = line.replace(user_key, new_user_key)
-                    replaced_content = replaced_content + new_line + "\n"
-                file.close()
+    with open("user_keys.txt") as user_keys:
+        keys = user_keys.read()
 
-                write_file = open("", "w")
-                write_file.write(replaced_content)
-                write_file.close()
+        if sender_key and reciver_key in keys:
+            if amount > sender_balance:
+                print("Error not enough funds; ", sender_balance, " is smaller then ", amount)
+            else:
+                pass
+        else:
+            print("Invalid sender or reciver key")
 
-def generate_new_user_key(username, password, wallet_balance):
-    combined_data = username + " " + password + " " + wallet_balance
-    new_user_key = hashlib.sha256(combined_data.encode()).hexdigest()
-    print("NEW USER KEY:")
-
-generate_user_identification()
+send()

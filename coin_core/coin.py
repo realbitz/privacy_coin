948e9853b51857e1b52c210165fe3b9afa89962d
M <112826187+m-the-coder@users.noreply.github.com>
2023-03-02 13:32:14 +0000

    finnished the mining function

--- coin_core/coin.py
+++ coin_core/coin.py
@@ -3,75 +3,69 @@ import time
 
 def create_wallet(username, password):
     wallet_balance = "0"
-    user_key_prefix = hashlib.sha256((username + password).encode()).decode()
-    user_key = user_key_prefix + wallet_balance
-    private_key = hashlib.sha256(user_key.encode()).decode()
+    wallet_address_prefix = hashlib.sha256((username + password).encode()).hexdigest()
+    wallet_address = wallet_address_prefix + "_" + wallet_balance
 
-    print("Your user key: ", user_key)
-    print("Your private key: ", private_key)
+    print("Your wallet address: ", wallet_address)
 
     f = open("userdata.txt", "a")
-    f.write(user_key + "." + private_key + "\n")
+    f.write(wallet_address + "\n")
     f.close()
 
-def send(sender_key, sender_private_key, receiver_key, amount):
-    with open("userdata.txt") as file:
-        contents = file.readlines()
+def send(sender_address, receiver_address, amount):
+    with open("userdata.txt") as keys:
+        content = keys.read()
+        if sender_address and receiver_address in content:
+            sender_balance = int(sender_address.split("_")[1])
+            if amount <= sender_balance:
 
-    sender_index = -1
-    receiver_index = -1
-    for i, line in enumerate(contents):
-        user_key, private_key = line.strip().split(".")
-        if sender_key == user_key and sender_private_key == private_key:
-            sender_index = i
-        elif receiver_key == user_key:
-            receiver_index = i
+                sender_balance -= amount
+                receiver_balance = int(receiver_address.split("_")[1])
+                receiver_balance += amount
 
-    if sender_index == -1 or receiver_index == -1:
-        print("Invalid data either: (sender key, sender priv key or receiver key)")
-        return
+                sender_address = sender_address.split("_")[0] + "_" + str(sender_balance)
+                receiver_address = receiver_address.split("_")[0] + "_" + str(receiver_balance)
 
-    sender_user_key, sender_private_key = contents[sender_index].strip().split(".")
-    receiver_user_key, receiver_private_key = contents[receiver_index].strip().split(".")
+                print("Transaction successful.")
 
-    sender_balance = int(sender_user_key.split("_")[1])
-    receiver_balance = int(receiver_user_key.split("_")[1])
+                content = content.replace(sender_address.split("_")[0] + "_" + str(sender_balance + amount), sender_address)
+                content = content.replace(receiver_address.split("_")[0] + "_" + str(receiver_balance - amount), receiver_address)
 
-    if amount > sender_balance:
-        print("Not enough balance")
-        return
-
-    sender_balance -= amount
-    receiver_balance += amount
-
-    new_sender_user_key = f"{sender_user_key.split('_')[0]}_{sender_balance}"
-    new_receiver_user_key = f"{receiver_user_key.split('_')[0]}_{receiver_balance}"
-
-    new_sender_key = new_sender_user_key + sender_private_key
-    new_receiver_key = new_receiver_user_key + receiver_private_key
-
-    contents[sender_index] = new_sender_key + "\n"
-    contents[receiver_index] = new_receiver_key + "\n"
-
-    with open("userdata.txt", "w") as file:
-        file.writelines(contents)
-
-    print("Transaction successful")
+                with open("userdata.txt", "w") as keys:
+                    keys.write(content)
+            else:
+                print("Insufficient balance.")
+        else:
+            print("Invalid wallet addresses.")
 
 def mine(miner_key):
-    nonce = 0
-    
-    with open("userdata.txt") as file:
-        contents = file.read()
-        if miner_key in contents:
-            while True:
-                current_time = int(time.time())
-                message = str(nonce) + str(current_time)
-                hash_result = hashlib.sha256(message.encode('utf-8')).hexdigest()
-                if hash_result[:2] == "000":
-                    print("Hash found: " + hash_result)
-                    send("MASTERNODE_10000", "masternodeprivkey", miner_key, 1)
-                    print("REWARD RECIVED")
-                nonce += 1
-        else:
-            print("Invalid Miner key")
+    target_prefix = '0' * 5
+    reward = 1
+    while True:
+        message = str(time.time()).encode()
+        sha256_hash = hashlib.sha256(message).hexdigest()
+
+        if sha256_hash.startswith(target_prefix):
+            print("Hash: " + sha256_hash)
+
+            with open("userdata.txt") as keys:
+                content = keys.read()
+                addresses = content.strip().split('\n')
+                index = addresses.index(miner_key)
+
+                balance = int(addresses[index].split('_')[1])
+                new_balance = balance + reward
+                new_address = addresses[index].split('_')[0] + '_' + str(new_balance)
+                addresses[index] = new_address
+
+                content = '\n'.join(addresses)
+
+            with open("userdata.txt", "w") as keys:
+                keys.write(content)
+            print("REWARD GRANTED:", reward)
+
+            miner_key = new_address
+
+            
+
+mine("22d324416d6c2513dca5b6e69d50eac29777a11f9be42d877704741474756ffc_0")

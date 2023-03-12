9a3634c18b1927a62d5186df3755955f4889d39f
Makko <112826187+realbitz@users.noreply.github.com>
2023-03-12 15:31:18 +0100

    miner separation

--- coin_core/coin.py
+++ coin_core/coin.py
@@ -37,35 +37,3 @@ def send(sender_address, receiver_address, amount):
                 print("Insufficient balance.")
         else:
             print("Invalid wallet addresses.")
-
-def mine(miner_key):
-    target_prefix = '0' * 5
-    reward = 1
-    while True:
-        message = str(time.time()).encode()
-        sha256_hash = hashlib.sha256(message).hexdigest()
-
-        if sha256_hash.startswith(target_prefix):
-            print("Hash: " + sha256_hash)
-
-            with open("userdata.txt") as keys:
-                content = keys.read()
-                addresses = content.strip().split('\n')
-                index = addresses.index(miner_key)
-
-                balance = int(addresses[index].split('_')[1])
-                new_balance = balance + reward
-                new_address = addresses[index].split('_')[0] + '_' + str(new_balance)
-                addresses[index] = new_address
-
-                content = '\n'.join(addresses)
-
-            with open("userdata.txt", "w") as keys:
-                keys.write(content)
-            print("REWARD GRANTED:", reward)
-
-            miner_key = new_address
-
-            
-
-mine("22d324416d6c2513dca5b6e69d50eac29777a11f9be42d877704741474756ffc_0")
